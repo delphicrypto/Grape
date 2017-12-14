69fe56c2c069ed0530cc246bf2f96b1a7d67c3d2
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2017-12-14 09:25:36 -0500

    add files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gnunicorn App/server:app
---
+++ tangle.py
@@ -0,0 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+#define tangle
+
+#define Tx
+
+#evaluate a paper
+
+if __name__ == "__main__":
+    pass
