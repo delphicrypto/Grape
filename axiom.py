0cf80325f98261dfc9463cd87f01d1d206a07ace
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2017-12-14 19:16:06 -0500

    tangle update done. need to fix loading of tangle.json in javascript

---
+++ axiom.py
@@ -0,0 +1,32 @@
+import time
+import json
+
+import networkx as nx
+from networkx.readwrite import json_graph
+from fastecdsa import keys, curve
+
+def keypair():
+    priv_key = keys.gen_private_key(curve.secp256k1)
+    pub_key = keys.get_public_key(priv_key, curve.secp256k1)
+    return priv_key, pub_key
+    
+def axiom_paper():
+    """
+    Need to change key to actual public key.
+    """
+    #allows self loops need to fix this
+    G = nx.DiGraph()
+    node_data = {
+        'link_1': '000000000000000000000000000000000',
+        'link_2': '000000000000000000000000000000000',
+        'pub': keypair()[1].x,
+        'timestamp': time.time()}
+    G.add_node('23b391d38e4827864257263921fae0fb', attr_dict=node_data)
+    graph_data = json_graph.node_link_data(G)
+    json.dump(graph_data, open("tangle.json", "wb"))
+
+    pass
+
+if __name__ == "__main__":
+    # axiom_paper()
+    json.load(open("tangle.json", "rb"))
--- server.py
+++ server.py
@@ -3,10 +3,14 @@ from os import path
 import sys
 import logging
 import hashlib
+import json
 
 from flask import Flask
 from flask import render_template
 from flask import send_from_directory
+from flask import request
+
+from networkx.readwrite import json_graph
 
 app = Flask(__name__)
 
@@ -39,7 +43,73 @@ def load_papers():
     return (Paper(path.join("Papers", p)) for p in papers if not \
         p.startswith("."))
 
+def load_tangle():
+    """
+    Reads tangle.json and loads as networkx object. 
+
+    Returns:
+    Graph: networkx graph object of tangle.
+    """
+    graph_data = json.load(open("tangle.json", "rb"))
+    tangle = json_graph.node_link_graph(graph_data)
+    return tangle
+
+def update_tangle(tangle, paper, link_1, rev_1,  link_2, rev_2, pub):
+    """
+    Adds a paper to the tangle.
+
+    Args:
+    tangle: tangle graph
+    paper: Paper object
+    link_1: hash of paper to review
+    rev_1: review of paper 1
+    link_2: hash of other paper to review
+    rev_2: review of paper 2
+    pub: public key of submitter
+    """
+    data = {
+        'link_1': link_1,
+        'link_2': link_2,
+        'pub': pub
+            }
+    #add node to graph
+    tangle.add_node(paper, attr_dict=data)
+    tangle.add_edge(paper, link_1, rev=rev_1)
+    tangle.add_edge(paper, link_1, rev=rev_1)
+
+    #dump new version of tangle
+    graph_data = json_graph.node_link_data(G)
+    json.dump(graph_data, open("tangle.json", "wb"))
+
+    pass
+
 @app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/submit')
+def submit_form():
+    return render_template('submit.html')
+
+@app.route('/submitted', methods=['POST', 'GET'])
+def submit_paper():
+    if request.method == "POST":
+        result = request.form
+        tangle = load_tangle()
+
+        paper_hash = result['hash']
+        pub = result['pub_key']
+        link_1 = result['link_1']
+        link_2 = result['link_2']
+        rev_1 = result['rev_1']
+        rev_2 = result['rev_2']
+
+        update_tangle(tangle, paper_hash, link_1, rev_1, \
+            link_2, rev_2, pub)
+
+        return render_template("submitted.html", result=result)
+    pass
+
 @app.route('/papers')
 def paper_list():
     papers = load_papers()
@@ -48,9 +118,13 @@ def paper_list():
 @app.route('/Papers/<p_id>')
 def send_pdf(p_id=None):
     return send_from_directory('Papers', p_id )
+
 @app.route('/tangle')
 def tangle():
-    return render_template('tangle.html')
+    """
+    Needs to take tangle graph object
+    """
+    return render_template('force.html')
 
 if __name__ == "__main__":
     app.run()
